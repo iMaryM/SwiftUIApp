72a9611cccff070da6d1e1d9700bc734c7091733
marym <maria_manzhos@tut.by>
2021-11-13 20:50:57 +0300

    initial commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Мария Манжос on 13.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
