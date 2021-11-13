91341d0844fbb6daf59b81b92078308c65f71eba
marym <maria_manzhos@tut.by>
2021-11-13 22:48:19 +0300

    added textfields "Login", "Password" and buttons "Login", "Registration"

--- SwiftUIApp/ContentView.swift
+++ SwiftUIApp/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var login: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Image("mainScreen")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            VStack {
+                Text("Login")
+                    .padding(10)
+                TextField("Enter login", text: $login)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.horizontal, 150.0)
+                Text("Password")
+                    .padding(10)
+                TextField("Password", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.horizontal, 150.0)
+                
+                HStack {
+                    Button("LogIn") {
+                        print("Login")
+                    }
+                    .padding(10)
+                    .buttonStyle(.bordered)
+                    .tint(Color.green)
+                    Button("Registration") {
+                        print("Registration")
+                    }
+                    .padding(10)
+                    .buttonStyle(.bordered)
+                    .tint(Color.green)
+                }
+            }
+
+        }
+
     }
 }
 
